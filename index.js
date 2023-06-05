31b87570df33470825a7facd3b8443d7b8afa08d
mirandalu2020 <yuanling1007@hotmail.com>
2023-06-05 15:27:15 -0700

    plant status created

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const dynamoose = require('dynamoose');
 
-const personSchema = new dynamoose.Schema({
-  id: String,
-  name: String, 
-  age: String, 
-  height: String,
+const plantSchema = new dynamoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  temperature: {
+    type: Number,
+  },
+  moisture: {
+    type: Number,
+  },
+  timestamp: {
+      type:String,
+      required: true,
+  }
 });
 
-const PersonModel = dynamoose.model('People', personSchema);
+const PlantModel = dynamoose.model('caterpillar-plant-status', plantSchema);
 
 exports.handler = async(event) =>{
   console.log('EVENT ', event);
-  let person = JSON.parse(event.body);
-  console.log('READING PERSON', person);
-  let returnPerson;
+  let plant = JSON.parse(event.body);
+  console.log('READING PLANT', plant);
+  let returnPlant;
   
   try{
-    returnPerson = await PersonModel.create(person);
+    returnPlant = await PlantModel.create(plant);
   }
   catch(e){
     console.error('ERROR: ', e);
@@ -24,7 +38,7 @@ exports.handler = async(event) =>{
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(`HELLO FROM NEW PERSON, ${JSON.stringify(returnPerson)}`),
+    body: JSON.stringify(`HELLO FROM NEW PLANT, ${JSON.stringify(returnPlant)}`),
 };
   return response;
 
